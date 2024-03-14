beaa8b86715c696bad0e52a1fbdea7cdebf89820
Brian VanLoo <brian.vanloo@gmail.com>
2024-03-13 17:37:37 -0700

    Add db seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,16 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+Node.find_or_create_by!(id: 130, parent_id: nil)
+Node.find_or_create_by!(id: 125, parent_id: 130)
+Node.find_or_create_by!(id: 2820230, parent_id: 125)
+Node.find_or_create_by!(id: 4430546, parent_id: 125)
+Node.find_or_create_by!(id: 5497637, parent_id: 4430546)
+
+Bird.find_or_create_by!(id: 1, node_id: 125)
+Bird.find_or_create_by!(id: 2, node_id: 130)
+Bird.find_or_create_by!(id: 3, node_id: 2820230)
+Bird.find_or_create_by!(id: 4, node_id: 4430546)
+Bird.find_or_create_by!(id: 5, node_id: 5497637)
+Bird.find_or_create_by!(id: 6, node_id: 5497637)
